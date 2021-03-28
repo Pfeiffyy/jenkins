e47ea21ab0210a45252bf98d29896953631816e3
uebboot <uebboot@gmail.com>
2021-03-28 11:56:00 +0200

    ww

---
+++ JenTest/src/Helper.java
@@ -0,0 +1,16 @@
+
+public class Helper {
+
+	
+	String name;
+
+	public String getName() {
+		name = "Desiderous Frolockan Mausitious";
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
